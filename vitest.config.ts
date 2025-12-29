5533f198ea0f3b0a50fe84f337b97af0bdfe8a76
Atif-Rafique <Iamatif910@gmail.com>
2025-12-29 15:55:35 +0500

    chore: updated code

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: [
+      exclude: [ 
         'node_modules/',
         'src/tests/',
         '**/*.d.ts',
